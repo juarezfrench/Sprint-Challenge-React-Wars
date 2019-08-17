5ae1ef1e509532574b22bbe7ebb08898d1dfd2a6
Mark French <markedfrench@gmail.com>
2019-08-17 15:54:08 -0500

    Styled the cards. Will tweak the styling and look at the stretch in a bit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import styled from 'styled-components';
-// import CharacterCard from "./CharacterCard"
+
+
 import './App.css';
 
 
@@ -39,36 +40,33 @@ console.error('App.js -> %c Problem getting data - check this: ', 'color: Fuchsi
     
     <section className="cardContainer">
           
-    <h1 className="Header">React Wars</h1>
+    <Title className="Header">React Wars</Title>
     <div className="cardMap">
   
       {data.map(card => {
   
        
 
-  return <CharacterCard>
-		
-    
- 			<div>
+  return  <Container>
+     
+        <Card>
+        <Content>
+
        <Name>{card.name} </Name>
        <Intro className="intro"> About {card.name} </Intro>
-       
+   
        <BirthYear className = "BirthYear">Near as we can tell, {card.name} was born or came into being on {card.birth_year}
        </BirthYear>
-       
       
-       
-       <Content className="height">
 
-          {card.name} is about {Math.round(card.height*.0328084)} feet tall.
-
+         <Height> {card.name} is about {Math.round(card.height*.0328084)} feet tall</Height>
+         
       </Content>
-    
-      </div>
- 		
- 			</CharacterCard>
+      </Card>
+     
+      </Container>
       })}
-      
+
     </div>
     </section>
   
@@ -80,29 +78,67 @@ console.error('App.js -> %c Problem getting data - check this: ', 'color: Fuchsi
 export default App
 // console.log('App.js -> %cApp:', 'color: DarkCyan', App)
 
-const Name = styled.p`
 
-   display: flex;
-   justify-content: center;   
- 
-  color: DimGray;
+const Container = styled.div `
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+grid-template-rows: auto;
+
+
+`
+const Card = styled.div`
+// display: inline-flexbox;
+// width: 30%;
+color: MidnightBlue;
+background: #50B4D8;
+opacity: .8;
+border: 2px solid Gray;
+  
+`;
+
+const Title = styled.h1`
+
+ text-align: center;
+      font-size: 4.5em; 
+  color: #767676;
   font-family: Baskerville;
   text-transform: uppercase;
-  font-size: large;
+  
 `;
+
+
+
+const Name = styled.h1`
+
+      
+  color: #ff0000;
+  font-family: Baskerville;
+  text-transform: uppercase;
+
+`;
+
 
 
 const BirthYear=styled.p`
 font-size="medium;
-display: flex;
-justify-content: center;   
+ 
 color: DimGray;
 text-transform: uppercase;
 font-family: Baskerville;
 `
+const Height=styled.p`
+// text-align: center;
+color: DimGray;
+text-transform: uppercase;
+font-weight: bold;
+font-family: 'League Spartan Bold';
+    font-weight:normal;
+    font-style:normal;
+`
+
 
 const Intro=styled.p`
-text-align: center;
+// text-align: center;
 color: DimGray;
 text-transform: uppercase;
 font-weight: bold;
@@ -113,22 +149,22 @@ font-family: 'League Spartan Bold';
 
 
 const Content=styled.p `
-text-align: center;
+
 color: Gray;
 text-align: left;
 margin: 0 5% 0 5%;
 padding-bottom: 5%;
 
 `
-const CharacterCard = styled.div `
+// const CharacterCard = styled.div `
 
-display:flex;
-flex-direction: column;
-justify-content: center;
-background: Black;
-margin: 2% 10% 0% 10%;
+// display:flex;
+// flex-direction: column;
+// justify-content: center;
+// background: Black;
+// margin: 2% 10% 0% 10%;
 
-`
+// `
 
 
   
